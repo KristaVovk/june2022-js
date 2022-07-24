7140ec16bcec060f454d4c2dd2ed6be085017a08
KristaVovk <kristavovk@gmail.com>
2022-07-24 11:26:00 +0300

    HT1

--- Lesson1 HT/main.js
+++ Lesson1 HT/main.js
@@ -1,78 +1,78 @@
-// let a = 'hello'
-// console.log(a);
-// alert(a);
-// document.write(a);
-//
-// let b = 'owu'
-// console.log(b);
-// alert(b);
-// document.write(b);
-//
-// let c = 'com'
-// console.log(c);
-// alert(c);
-// document.write(c);
-//
-// let d = 'ua'
-// console.log(d);
-// alert(d);
-// document.write(d);
-//
-// let e = 1
-// console.log(e);
-// alert(e);
-// document.write(e);
-//
-// let f = 10
-// console.log(f);
-// alert(f);
-// document.write(f);
-//
-// let g = -999
-// console.log(g);
-// alert(g);
-// document.write(g);
-//
-// let h = 123
-// console.log(h);
-// alert(h);
-// document.write(h);
-//
-// let i = 3.14
-// console.log(i);
-// alert(i);
-// document.write(i);
-//
-// let j = 2.7
-// console.log(j);
-// alert(j);
-// document.write(j);
-//
-// let k = 16
-// console.log(k);
-// alert(k);
-// document.write(k);
-//
-// let l = true
-// console.log(l);
-// alert(l);
-// document.write(l);
-//
-// let m = false
-// console.log(m);
-// alert(m);
-// document.write(m);
-//
-// let text = `${a} ${b} ${c} ${d} ${e} ${f} ${g} ${h} ${i} ${j} ${k} ${l} ${m}`
-// console.log(text)
-//
-// let firstName = 'Kristina';
-// let middleName = "Sergiivna";
-// let lastName = 'Vovk';
-// let person1 = firstName + " " + middleName + " " + lastName;
-// let person2 = `${firstName} ${middleName} ${lastName}`;
-// console.log(person1);
-// console.log(person2);
+let a = 'hello'
+console.log(a);
+alert(a);
+document.write(a);
+
+let b = 'owu'
+console.log(b);
+alert(b);
+document.write(b);
+
+let c = 'com'
+console.log(c);
+alert(c);
+document.write(c);
+
+let d = 'ua'
+console.log(d);
+alert(d);
+document.write(d);
+
+let e = 1
+console.log(e);
+alert(e);
+document.write(e);
+
+let f = 10
+console.log(f);
+alert(f);
+document.write(f);
+
+let g = -999
+console.log(g);
+alert(g);
+document.write(g);
+
+let h = 123
+console.log(h);
+alert(h);
+document.write(h);
+
+let i = 3.14
+console.log(i);
+alert(i);
+document.write(i);
+
+let j = 2.7
+console.log(j);
+alert(j);
+document.write(j);
+
+let k = 16
+console.log(k);
+alert(k);
+document.write(k);
+
+let l = true
+console.log(l);
+alert(l);
+document.write(l);
+
+let m = false
+console.log(m);
+alert(m);
+document.write(m);
+
+let text = `${a} ${b} ${c} ${d} ${e} ${f} ${g} ${h} ${i} ${j} ${k} ${l} ${m}`
+console.log(text)
+
+let firstName = 'Kristina';
+let middleName = "Sergiivna";
+let lastName = 'Vovk';
+let person1 = firstName + " " + middleName + " " + lastName;
+let person2 = `${firstName} ${middleName} ${lastName}`;
+console.log(person1);
+console.log(person2);
 
 let a1 = 100;
 console.log(typeof a1)
