4a303411f768ae7abd22040b62d4d625db846023
KristaVovk <kristavovk@gmail.com>
2022-07-27 20:54:46 +0300

    HT2 done

--- Lesson2 HT/main.js
+++ Lesson2 HT/main.js
@@ -13,59 +13,63 @@ console.log(agenda[8]);
 console.log(agenda[9]);
 
 // - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
-let book1 = {
-    title: 'Contagious: Why Things Catch On',
-    pageCount: 256,
-    genre: 'Marketing & Sales'
-};
+let book = [
+    book1 = {
+        title: 'Contagious: Why Things Catch On',
+        pageCount: 256,
+        genre: 'Marketing & Sales'
+    },
+
+    book2 = {
+        title: 'Building a StoryBrand: Clarify Your Message So Customers Will ',
+        pageCount: 240,
+        genre: 'Marketing & Sales'
+    },
+
+    book3 = {
+        title: 'Traffic Secrets',
+        pageCount: 352,
+        genre: 'Marketing & Sales'
+    }
+]
 console.log(book1)
-
-let book2 = {
-    title: 'Building a StoryBrand: Clarify Your Message So Customers Will ',
-    pageCount: 240,
-    genre: 'Marketing & Sales'
-};
-console.log(book2);
-
-let book3 = {
-    title: 'Traffic Secrets',
-    pageCount: 352,
-    genre: 'Marketing & Sales'
-};
+console.log(book2)
 console.log(book3)
 
 // - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
-let book3_1 = {
-    title: 'Contagious: Why Things Catch On',
-    pageCount: 256,
-    genre: 'Marketing & Sales',
-    author: {
-        name: 'Russell Brunson',
-        age: 42
+let books = [
+    book3_1 = {
+        title: 'Contagious: Why Things Catch On',
+        pageCount: 256,
+        genre: 'Marketing & Sales',
+        author: {
+            name: 'Russell Brunson',
+            age: 42
+        },
     },
-};
+
+    book2_1 = {
+        title: 'Building a StoryBrand: Clarify Your Message So Customers Will ',
+        pageCount: 240,
+        genre: 'Marketing & Sales',
+        author: {
+            name: 'Donald Miller',
+            age: 48
+        },
+    },
+
+    book1_1 = {
+        title: 'Contagious: Why Things Catch On',
+        pageCount: 256,
+        genre: 'Marketing & Sales',
+        author: {
+            name: 'Jonah Berger',
+            age: 28
+        }
+    }
+];
 console.log(book3_1.author);
-
-let book2_1 = {
-    title: 'Building a StoryBrand: Clarify Your Message So Customers Will ',
-    pageCount: 240,
-    genre: 'Marketing & Sales',
-    author: {
-        name: 'Donald Miller',
-        age: 48
-    },
-};
 console.log(book2_1.author);
-
-let book1_1 = {
-    title: 'Contagious: Why Things Catch On',
-    pageCount: 256,
-    genre: 'Marketing & Sales',
-    author: {
-        name: 'Jonah Berger',
-        age: 28
-    },
-};
 console.log(book1_1.author);
 // - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
 let banda = [
@@ -74,12 +78,12 @@ let banda = [
         username: 'Divisenko',
         password: 111
     },
-     {
+    {
         name: 'Oleksandra',
         username: 'Doroguncova',
         password: 222
     },
-     {
+    {
         name: 'Max',
         username: 'Boritko',
         password: 333
@@ -89,12 +93,12 @@ let banda = [
         username: 'Boritko',
         password: 444
     },
-     {
+    {
         name: 'Marina',
         username: 'Kunec',
         password: 555
     },
-     {
+    {
         name: 'Genya',
         username: 'Velichev',
         password: 666
@@ -104,12 +108,12 @@ let banda = [
         username: 'Virgesh',
         password: 777
     },
-     {
+    {
         name: 'Egor',
         username: 'Petrov',
         password: 888
     },
-     {
+    {
         name: 'Masha',
         username: 'Matveenko',
         password: 999
